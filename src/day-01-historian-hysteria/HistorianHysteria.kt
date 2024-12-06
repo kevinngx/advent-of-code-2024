a89ac0157637d0712c62904331f8799bf33ccb6c
Kevin Nguyen <kevinngx625@gmail.com>
2024-12-06 12:33:32 +1100

    Day 2

---
+++ src/day-01-historian-hysteria/HistorianHysteria.kt
@@ -0,0 +1,59 @@
+package `day-01-historian-hysteria`
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val INPUT_FILE_NAME = "src/day-01-historian-hysteria/input_data.txt"
+    // val INPUT_FILE_NAME = "src/day-01-historian-hysteria/test_data.txt"
+    listDistance(INPUT_FILE_NAME)
+    listSimilarity(INPUT_FILE_NAME)
+}
+
+fun listDistance(filename: String) {
+    val inputStream = File(filename).inputStream()
+    val listA = mutableListOf<Int>()
+    val listB = mutableListOf<Int>()
+    inputStream.bufferedReader().useLines { lines ->
+        lines.forEach { line ->
+            listA.add(line.split("   ")[0].toInt())
+            listB.add(line.split("   ")[1].toInt())
+        }
+    }
+    listA.sort()
+    listB.sort()
+    var runningDiff = 0
+    for (i in listA.indices) {
+        runningDiff += abs(listA[i] - listB[i])
+    }
+    println("List distance is $runningDiff")
+}
+
+fun listSimilarity(filename: String) {
+    val inputStream = File(filename).inputStream()
+    val mapA = mutableMapOf<Int, Int>()
+    val mapB = mutableMapOf<Int, Int>() //: HashMap<Int, Int>
+    inputStream.bufferedReader().useLines { lines ->
+        lines.forEach { line ->
+            val lineElems = line.split("   ")
+            val a = lineElems[0].toInt()
+            val b = lineElems[1].toInt()
+
+            if (mapA.containsKey(a)) {
+                mapA[a] = mapA[a]!! + 1
+            } else {
+                mapA[a] = 1
+            }
+            if (mapB.containsKey(b)) {
+                mapB[b] = mapB[b]!! + 1
+            } else {
+                mapB[b] = 1
+            }
+        }
+    }
+    var similarity = 0
+    for (item in mapA) {
+        similarity += item.key * item.value * ( if (mapB.contains(item.key)) mapB[item.key]!! else 0)
+    }
+    println("List similarity is $similarity")
+}
---
+++ src/day-02-red-nosed-reports/RedNosedReports.kt
@@ -0,0 +1,59 @@
+package `day-02-red-nosed-reports`
+
+import java.io.File
+import kotlin.math.abs
+
+enum class direction(val value: Int) {
+    INCREASING(1),
+    DECREASING(-1),
+    EQUAL(0)
+}
+
+fun main() {
+        val INPUT_FILE_NAME = "src/day-02-red-nosed-reports/input_data.txt"
+//         val INPUT_FILE_NAME = "src/day-02-red-nosed-reports/test_data.txt"
+
+        val inputStream = File(INPUT_FILE_NAME).inputStream()
+        var safeCount = 0
+        var expectedDirection : direction
+
+        inputStream.bufferedReader().forEachLine { line ->
+            val nums = line.split(" ")
+            if (nums.size < 2) {
+                safeCount++
+                return@forEachLine
+            } else {
+                expectedDirection = getDirection(nums[0].toInt(), nums[1].toInt())
+            }
+
+            var problemDampenerHasCharge = true
+            var prev = nums[0].toInt()
+
+            for (i in 1 until  nums.size) {
+                val curr = nums[i].toInt()
+                if (getDirection(prev, curr) != expectedDirection || abs(curr - prev) > 3) {
+                    // Check three options:
+                    // Remove previous
+
+                    // Remove current
+                    // Remove next
+
+
+                    return@forEachLine
+                }
+                prev = curr
+            }
+            safeCount++
+        }
+    println("Safe Count is $safeCount")
+}
+
+fun getDirection(a: Int, b: Int): direction {
+    if (a < b) {
+        return direction.INCREASING
+    } else if (a > b) {
+        return direction.DECREASING
+    } else {
+        return direction.EQUAL
+    }
+}
