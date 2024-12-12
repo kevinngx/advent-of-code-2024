716d44caf0336f5c352172a465702a9b73d5fb5f
Kevin Nguyen <kevinngx625@gmail.com>
2024-12-12 19:49:53 +1100

    Day 3

---
+++ src/day-03-mull-it-over/MullItOver.kt
@@ -0,0 +1,45 @@
+package `day-03-mull-it-over`
+
+import java.io.File
+
+class MullItOver {
+
+
+}
+
+fun main() {
+    val INPUT_FILE_NAME = "src/day-03-mull-it-over/input_data.txt"
+//    val INPUT_FILE_NAME = "src/day-03-mull-it-over/test_data.txt"
+    solve(INPUT_FILE_NAME)
+}
+
+fun solve(file: String) {
+    val inputStream = File(file).inputStream()
+    var output = 0
+    inputStream.bufferedReader().forEachLine { line ->
+        output += getLineValue(line)
+    }
+    println("Solution output = $output")
+}
+
+fun getLineValue(line: String): Int {
+    val matches = getMatches(line)
+    var lineValue = 0
+    for (match in matches) {
+        lineValue += parseMatchValue(match)
+    }
+    return lineValue
+}
+
+fun getMatches(string: String): List<String> {
+    val regex = Regex("mul\\(\\d+,\\d+\\)")
+    return regex.findAll(string).map { it.value }.toList()
+}
+
+fun parseMatchValue(string: String): Int {
+    // in form "mul(a,b)"
+    val parts = string.substring(4, string.length-1).split(',')
+    return mul(parts[0].toInt(), parts[1].toInt())
+}
+
+fun mul(a: Int, b: Int) = a * b
